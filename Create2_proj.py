a6b0295fec37a95d2b525e90c038c45528f3ee70
Chase Grund <chase.grund@udri.udayton.edu>
2023-03-22 14:59:03 -0400

    feat: Initial prototype for action queue and motion events

--- Create2_proj.py
+++ Create2_proj.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import enum
+import multiprocessing
+import select
+import threading
 # Version History
 # v1.0: Python2.7 -- 2015//05/27
 # v2.0: Update to Python3 -- 2020/04/01
@@ -388,6 +391,171 @@ class TetheredDriveApp(Tk):
             self.light_timer.stop()
             del self.light_timer
             self.light_timer = None
+
+
+class World:
+    """
+    World is an internal representation of the environment around the robot, intended to allow the bot to query for
+    information (eg, known obstacles, cost-to-go, metrics, etc.), and to allow the bot's experiences to update the
+    representation with new data
+    """
+
+    def __init__(self):
+        # min/max world extents, as viewed from above in 2 dimensions.  y-up is the along the inital forward direction
+        # of the robot
+        self.max_x = 0
+        self.min_x = 0
+        self.max_y = 0
+        self.min_y = 0
+        self.cur_x = 0
+        self.cur_y = 0
+
+        # statistics
+        # total distance traveled
+        self.total_dist = 0
+
+
+class MoveTimeAction:
+    def __init__(self, rvel, lvel, t):
+        self.rvel = rvel
+        self.lvel = lvel
+        self.total_t = t
+        self.elapsed_t = 0
+
+    def begin(self):
+        # send command to drive robot with the configured wheel velocities
+        print("B", end="")
+
+    def update(self, evt):
+        match evt.type:
+            case EVENT_TYPE.PAUSE:
+                # halt wheel motion
+                print("P", end="")
+            case EVENT_TYPE.RESUME:
+                # start wheel motion again
+                print("R", end="")
+            case EVENT_TYPE.TIMER:
+                print(".", end="")
+                self.elapsed_t += evt.delta_t
+                if 0 < self.total_t < self.elapsed_t:
+                    return UPDATE_RESULT.DONE
+            case _:
+                print("")
+        return UPDATE_RESULT.OK
+
+    def cancel(self):
+        if 0 < self.total_t <= self.elapsed_t:
+            return True
+
+    def duration(self):
+        return self.total_t
+
+
+class UPDATE_RESULT(enum.IntEnum):
+    OK = 0x01
+    DONE = 0x02
+    TERMINATE = 0x03
+
+
+class EVENT_TYPE(enum.IntEnum):
+    MESSAGE = 0x01
+    TIMER = 0x02
+    PAUSE = 0x03
+    RESUME = 0x04
+
+
+class EventMessage:
+    type = EVENT_TYPE.MESSAGE
+
+    def __init__(self, msg):
+        self.data = msg
+
+
+class EventTimer:
+    type = EVENT_TYPE.TIMER
+
+    def __init__(self):
+        self.data = ""
+
+
+class EventQueue:
+    def __init__(self):
+        self.r, self.w = multiprocessing.Pipe(False)
+
+    def put(self, evt):
+        self.w.send(evt)
+
+    def get(self):
+        return self.r.recv()
+
+    def fileno(self):
+        return self.r.fileno()
+
+
+class ActionSequence(threading.Thread):
+    """
+    ActionSequence manages a sequence of activities to be undertaken by the robot.  This is a serial list of actions,
+    such as move forward, turn, move back, beep, wait, etc.
+    Additionally, one or more EventQueues can be added, which can provide asynchronous signalling to the actions.  By
+    default, a single "heartbeat" event queue is created, which triggers regularly, sending a time delta to the active
+    action.  Other options for event queues include Cancel and Pause events.
+    Upon startup, The ActionSequence runs the first configured action, then waits for events, using a select.select()
+    call on all queues.
+    """
+    def __init__(self):
+        self.actions = []
+        self.current_action = 0
+        self.event_queues = []
+        self.exception_queues = []
+
+        self.heartbeat = EventQueue()
+        self.event_queues.append(self.heartbeat)
+
+        super().__init__(daemon=True)
+
+    def append(self, action):
+        self.actions.append(action)
+
+    def register_event(self, evt):
+        self.event_queues.append(evt)
+
+    def register_exception(self, exevt):
+        self.exception_queues.append(exevt)
+
+    def run(self):
+        while True:
+            evt_list, _, ex_list = select.select(self.event_queues, [], self.exception_queues)
+            for e in ex_list:
+                self.actions[self.current_action].cancel(e.get())
+                # we only need to respond to the first exception?
+                return
+
+            # Flag for whether we want to start the next action after all the events are processed. This is set to
+            # True if any action update returns Done.  We use a flag to ensure all current events are forwarded to
+            # the current action no matter what order they appear.
+            start_next_action = False
+            for e in evt_list:
+                if self.current_action > len(self.actions) - 1:
+                    # no current action, so just consume incoming events
+                    e.get()
+                    continue
+                # pass the event details to the current action for processing
+                result = self.actions[self.current_action].update(e.get())
+                match result:
+                    case UPDATE_RESULT.OK:
+                        # Nothing to do here, the update was all good
+                        continue
+                    case UPDATE_RESULT.DONE:
+                        # Action is done, get ready to move to the next one
+                        start_next_action = True
+                    case UPDATE_RESULT.TERMINATE:
+                        # We possibly want to post an exception here instead, for better cleanup
+                        return
+                    case _:
+                        print("UNDEFINED RESULT!")
+            # check our flag; if any of the events resulted in the action reaching completion, we want to go to the next
+            if start_next_action:
+                self.current_action += 1
 
 
 class KeyAction:
